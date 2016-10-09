01b6658bf17addaeee4aca4c25be3461480fcb96
Christian Pena <christianpena9@gmail.com>
2016-10-09 14:26:19 -0400

    add PUT todo

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/todos', function(req, res) {
 
 
     if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
-        return res.status(404).send();
+        return res.status(400).send();
     }
 
     // using trim to remove any extra spaces, either in beginning or end
@@ -104,6 +104,37 @@ app.delete('/todos/:id', function(req, res) {
         todos = _.without(todos, matchedTodo);
         res.json(matchedTodo); // returns the deleted item and a 200
     }
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId}); // returns the first json
+
+    var body = _.pick(req.body, 'description','completed');
+    var validAttributes = {};
+
+    if(!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if(body.hasOwnProperty('completed')) {
+        // something went bad, we got the property completed but it wasn't
+        // a boolean
+        return res.status(400).send();
+    }
+
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if(body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    matchedTodo = _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+
 });
 
 app.listen(PORT, function() {
