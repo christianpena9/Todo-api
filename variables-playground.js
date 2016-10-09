ada9397d37f5f0eb5b70ef3e4fd90cebdba14c50
Christian Pena <christianpena9@gmail.com>
2016-10-09 16:58:56 -0400

    add variable-playground area

---
+++ variables-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+//     name: 'Christian',
+//     age: 21
+// };
+//
+// function updateAge(obj) {
+//     obj = {
+//         name: 'Christian',
+//         age: 26
+//     }
+//     return obj
+// }
+// console.log(updateAge(person));
+// console.log(person);
+
+// array
+var grades = [15, 37];
+
+//calling push
+function addGrade(arr) {
+    arr.push(55);
+    debugger;
+}
+
+addGrade(grades);
+console.log(grades);
