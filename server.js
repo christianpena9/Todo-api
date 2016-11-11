4405433eafd16b52d1d962e695eb749469aa54e0
Christian Pena <christianpena9@gmail.com>
2016-11-11 11:28:37 -0500

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js')
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -74,20 +75,25 @@ app.post('/todos', function(req, res) {
     // Alternatively accepts a predicate indicating which keys to pick.
     var body = _.pick(req.body, 'description','completed');
 
+    db.todo.create(body).then(function(todo) {
+        res.json(todo.toJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
 
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
-        return res.status(400).send();
-    }
-
-    // using trim to remove any extra spaces, either in beginning or end
-    body.description = body.description.trim();
-
-    body.id = todoNextId;
-    todoNextId += 1;
-
-    todos.push(body);
-
-    res.json(body);
+    // if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+    //     return res.status(400).send();
+    // }
+    //
+    // // using trim to remove any extra spaces, either in beginning or end
+    // body.description = body.description.trim();
+    //
+    // body.id = todoNextId;
+    // todoNextId += 1;
+    //
+    // todos.push(body);
+    //
+    // res.json(body);
 });
 
 /*for each loop*/
@@ -155,6 +161,8 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-app.listen(PORT, function() {
-    console.log('Express listening on port: ' + PORT + '!');
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log('Express listening on port: ' + PORT + '!');
+    });
 });
