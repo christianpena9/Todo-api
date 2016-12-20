83d6b670f57eefa64d8c6cba1b4aee1ff51d4331
Christian Pena <christianpena9@gmail.com>
2016-12-19 23:44:05 -0500

    Update PUT /todos/:id to use sequelize

--- models/todo.js
+++ models/todo.js
@@ -1,16 +1,31 @@
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('todo', {
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 250]
-            }
-        },
-        completed: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            defaultValue: false
-        }
-    });
-}
+	return sequelize.define('todo', {
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 250]
+			}
+		},
+		completed: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	}, {
+		validate: {
+			descriptionIsString: function() {
+				if (!_.isString(this.description)) {
+					throw new Error('Description must be string.')
+				}
+			},
+			completedIsBoolean: function() {
+				if (!_.isBoolean(this.completed)) {
+					throw new Error('Completed must be boolean.')
+				}
+			}
+		}
+	});
+};
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
-var db = require('./db.js')
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -184,32 +184,30 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId}); // returns the first json
-
     var body = _.pick(req.body, 'description','completed');
-    var validAttributes = {};
+    var attributes = {};
 
-    if(!matchedTodo) {
-        return res.status(404).send();
+    if(body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
     }
 
-    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if(body.hasOwnProperty('completed')) {
-        // something went bad, we got the property completed but it wasn't
-        // a boolean
-        return res.status(400).send();
+    if(body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
 
-    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if(body.hasOwnProperty('description')) {
-        return res.status(400).send();
-    }
-
-    matchedTodo = _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
-
+    db.todo.findById(todoId).then(function(todo) {
+        if(todo) {
+            todo.update(attributes).then(function(todo) {
+                res.json(todo.toJSON());
+            }, function(e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).send();
+        }
+    }, function() {
+        res.status(500).send();
+    });
 });
 
 db.sequelize.sync().then(function() {
