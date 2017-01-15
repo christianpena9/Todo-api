4936c623accb146d3ae0197109b627bbb724f113
Christian Pena <christianpena9@gmail.com>
2017-01-14 23:16:49 -0500

    Adding todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -20,11 +20,39 @@ var Todo = sequelize.define('todo', {
     }
 });
 
+var User = sequelize.define('user', {
+    email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo)
+
 sequelize.sync({
     //force: true
 }).then(function() {
     console.log('Everything is synced');
 
+    User.findById(1).then(function(user) {
+        user.getTodos({ where: {completed: true} }).then(function(todos) {
+            todos.forEach(function(todo) {
+                console.log(todo.toJSON());
+            });
+        });
+    });
+
+    // User.create({
+    //     email: 'christianpena9@gmail.com'
+    // }).then(function() {
+    //     return Todo.create({
+    //         description: 'Clean the yard.'
+    //     });
+    // }).then(function(todo) {
+    //     User.findById(1).then(function(user) {
+    //         user.addTodo(todo);
+    //     });
+    // });
+
+/*
     return Todo.findById(2).then(function(todo) {
         if(todo) {
             console.log(todo.toJSON());
@@ -32,7 +60,7 @@ sequelize.sync({
             console.log('Todo not found!');
         }
     });
-
+*/
     // Todo.create({
     //     description: 'Walk my dog',
     //     completed: false
